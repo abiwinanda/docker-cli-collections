26d89a3315c22fd35491fa1406dc08ce5a438a47
abiwinanda <abiwinanda@outlook.com>
2022-06-12 12:38:28 +0800

    add docker build commands

---
+++ build-image.sh
@@ -0,0 +1,11 @@
+# Build image (in current DIR). By default this will look for "Dockerfile"
+docker build .
+
+# Build image (in current DIR) with custom name or tag
+docker build -t ${IMAGE_NAME} .
+docker build -t ${IMAGE_NAME}:${IMAGE_VERSION} . # often we use ":" to separate the image name with the version
+
+# Build image from a custom Dockerfile. e.g:
+# docker build -f Dockerfile.local .
+# docker build -f ./DirectoryName/Dockerfile .
+docker build -f ${DOCKERFILE_PATH} . 
