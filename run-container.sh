a10208e2bc999c72ae04e6751ffd00687cd9222f
abiwinanda <abiwinanda@outlook.com>
2022-06-12 12:47:04 +0800

    add run container with volume mount command

--- run-container.sh
+++ run-container.sh
@@ -12,4 +12,10 @@ docker container run -p ${HOST_PORT}:${CONTAINER_PORT} -d ${IMAGE_NAME}
 
 # Run docker container with env (use --env flag)
 # Use --env flag more than once if you want to set multiple envs
-docker container run -d --env ${ENV_NAME}={ENV_VALUE} ${IMAGE_NAME} # e.g docker container run -d --env GOOGLE_APPLICATION_CREDENTIALS=/gcp/config/application_default_credentials.json MyApp
+docker container run -d --env ${ENV_NAME}={ENV_VALUE} ${IMAGE_NAME} # e.g docker container run -d --env GOOGLE_APPLICATION_CREDENTIALS=/gcp/config/application_default_credentials.json MyAppImage
+
+# Run docker container with volume mount
+docker container run -d -v "${HOST_DIR}:${CONTAINER_DIR}" ${IMAGE_NAME} # e.g docker container run -d -v "$HOME/.config/gcloud:/gcp/config" MyAppImage
+
+# Run docker container with read only volume mount
+docker container run -d -v "${HOST_DIR}:${CONTAINER_DIR}:ro" ${IMAGE_NAME} # e.g docker container run -d -v "$HOME/.config/gcloud:/gcp/config:ro" MyAppImage
