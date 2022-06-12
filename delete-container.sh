3c090cce24b0447ce69fccd8f0f56ff548d307dc
abiwinanda <abiwinanda@outlook.com>
2022-06-12 12:56:17 +0800

    delete all docker containers

--- delete-container.sh
+++ delete-container.sh
@@ -3,3 +3,6 @@ docker container rm ${CONTAINER_ID}
 
 # Force delete docker container
 docker container rm ${CONTAINER_ID} --force
+
+# Delete all docker containers
+docker container rm $(docker ps -a -q)
